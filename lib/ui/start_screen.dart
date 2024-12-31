997344ab560d707171e0b892736ef1445215cade
Awais khan <116676945+AwaisKhan10@users.noreply.github.com>
2024-12-31 13:25:11 +0500

    changes

--- lib/ui/start_screen.dart
+++ lib/ui/start_screen.dart
@@ -10,6 +10,9 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      ///
+      /// Start Body
+      ///
       body: Padding(
         padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 70),
         child: Column(
